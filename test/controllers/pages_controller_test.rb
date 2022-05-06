5eac37acbe7c56506dd752c863b0828876367ce8
jovejar <jovejar@gmail.com>
2022-05-06 17:56:31 -0400

    prueba 06 de mayo

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get team" do
+    get pages_team_url
+    assert_response :success
+  end
+
+end
